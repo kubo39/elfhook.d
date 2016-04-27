6e4c4b6395f2972d15300af7840b7fe5dce57ab4
kubo39 <kubo39@gmail.com>
2016-04-27 18:22:40 +0900

    Using dstyle.

--- example/source/app.d
+++ example/source/app.d
@@ -2,22 +2,22 @@ import elfhook;
 
 extern(C)
 {
-  void original();
+    void original();
 }
 
 void myputs()
 {
-  import std.stdio : writeln;
-  writeln("monkey patch.");
+    import std.stdio : writeln;
+    writeln("monkey patch.");
 }
 
 void main()
 {
-  import std.file : getcwd;
-  auto so = getcwd() ~ "/liboriginal.so\0";
-  const char* filename = so.ptr;
-  const char* funcName = "puts\0".ptr;
-  auto address = hook(filename, funcName, &myputs);
-  assert(address !is null);  // Sucess for monkey-patching.
-  original();
+    import std.file : getcwd;
+    auto so = getcwd() ~ "/liboriginal.so\0";
+    const char* filename = so.ptr;
+    const char* funcName = "puts\0".ptr;
+    auto address = hook(filename, funcName, &myputs);
+    assert(address !is null);  // Sucess for monkey-patching.
+    original();
 }
